f62b6cad4b5f7baf481104456fe8dffe45fef617
calixlock <calixlock@naver.com>
2025-02-19 18:01:20 +0900

    easyFlutter04 complete appBar setting & options

--- 250219_codingApple_easyFlutter/easyflutter/lib/main.dart
+++ 250219_codingApple_easyFlutter/easyflutter/lib/main.dart
@@ -13,23 +13,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
-              title: Text("AppBar"),
-            ),
-            body: Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                // width: 50,
-                width: double.infinity,
-                height: 50,
+              // 앱바 왼쪽에 위치할 것
+              leading: Icon(Icons.star),
+              // 앱바 오른쪽에 위치할 것
+              actions: [
+                Icon(Icons.zoom_out_map),
+                Icon(Icons.account_box_outlined),
+              ],
 
-                // margin: EdgeInsets.all(20),
-                // margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                // padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black),
-                  color: Colors.blue,
-                ),
-                child: Text("data"),
+              title: Text("AppBar"),
+              backgroundColor: Colors.blue,
+            ),
+            body: SizedBox(
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'hello',
+                    // style: TextStyle(color: Colors.red),
+                    // style: TextStyle(color: Color.fromRGBO(r, g, b, opacity)),
+                    style: TextStyle(
+                        color: Color(0xffaaaaaa),
+                        fontSize: 50,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  TextButton(onPressed: () {}, child: Text('textBtn')),
+                  ElevatedButton(onPressed: () {}, child: Text('elevatedBtn')),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.star))
+                ],
               ),
             )));
   }
