f2cb2d8d100559febb7be069b61ad4d370d8bfb2
calixlock <calixlock@naver.com>
2025-03-06 00:56:01 +0900

    dart code test

--- 250306_flutter_nodejs/00_test.dart
+++ 250306_flutter_nodejs/00_test.dart
@@ -10,11 +10,20 @@ class Person {
   }
 }
 
-void main() {
-  //create instance
-  var alice = Person("Alice", 30);
-  var kim = Person("Kim", 20);
-  //call method
-  kim.greet();
-  alice.greet();
+// async
+Future<String> fetchUserData() {
+  return Future.delayed(Duration(seconds: 2), () => 'User data');
+}
+
+void main() async {
+  // //create instance
+  // var alice = Person("Alice", 30);
+  // var kim = Person("Kim", 20);
+  // //call method
+  // kim.greet();
+  // alice.greet();
+
+  // fetchUserData 완료까지 대기
+  String data = await fetchUserData();
+  print(data);
 }
